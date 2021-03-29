4e9199d04f9a7fde91e48c84b530d383ae19aa27
Ralf S. Engelschall <rse@engelschall.com>
2021-03-29 10:01:34 +0200

    start signing the distribution

--- npm-package.js
+++ npm-package.js
@@ -6,11 +6,14 @@
 
 /*  external requirements  */
 const os        = require("os")
+const fs        = require("fs")
 const path      = require("path")
 const glob      = require("glob")
 const shell     = require("shelljs")
 const execa     = require("execa")
 const zip       = require("cross-zip")
+const DSIG      = require("dsig")
+const PromptPW  = require("prompt-password")
 
 /*  establish asynchronous environment  */
 ;(async () => {
@@ -27,6 +30,25 @@ const zip       = require("cross-zip")
     for (const file of remove)
         shell.rm("-rf", file)
 
+    /*  helper function for digitally signing distribution artifact  */
+    const sign = async (zipfile) => {
+        console.log("++ generating digital signature for ZIP distribution archive")
+        const prompt = new PromptPW({
+            type:    "password",
+            message: "Password",
+            name:    "password"
+        })
+        const passPhrase = await prompt.run()
+        const sigfile = zipfile.replace(/\.zip$/, ".sig")
+        const payload = await fs.promises.readFile(zipfile, { encoding: null })
+        const privateKey = await fs.promises.readFile(
+            path.join(os.homedir(), ".dsig", "Vingester.prv"), { encoding: "utf8" })
+        const signature = await DSIG.sign(payload, privateKey, passPhrase)
+        await fs.promises.writeFile(sigfile, signature, { encoding: "utf8" })
+        const publicKey = await fs.promises.readFile("npm-package.pk", { encoding: "utf8" })
+        await DSIG.verify(payload, signature, publicKey)
+    }
+
     /*   package according to platform...  */
     const electronbuilder = path.resolve(path.join("node_modules", ".bin", "electron-builder"))
     if (os.platform() === "win32") {
@@ -41,6 +63,7 @@ const zip       = require("cross-zip")
         zip.zipSync(
             path.join(__dirname, "dist/Vingester.exe"),
             path.join(__dirname, "dist/Vingester-win-x64.zip"))
+        await sign("dist/Vingester-win-x64.zip")
     }
     else if (os.platform() === "darwin") {
         /*  run Electron-Builder to package the application  */
@@ -55,6 +78,7 @@ const zip       = require("cross-zip")
         zip.zipSync(
             path.join(__dirname, "dist/Vingester.app"),
             path.join(__dirname, "dist/Vingester-mac-x64.zip"))
+        await sign("dist/Vingester-mac-x64.zip")
     }
     else if (os.platform() === "linux") {
         /*  run Electron-Builder to package the application  */
@@ -68,6 +92,7 @@ const zip       = require("cross-zip")
         zip.zipSync(
             path.join(__dirname, "dist/Vingester"),
             path.join(__dirname, "dist/Vingester-lnx-x64.zip"))
+        await sign("dist/Vingester-lnx-x64.zip")
     }
 })().catch((err) => {
     console.log(`** package: ERROR: ${err}`)
